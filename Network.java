82fd8a22b5580f741be672ed19e724d0343f565c
jamespeterthornton <jamespeterthornton@gmail.com>
2014-11-02 18:47:41 -0500

    Added Random Initialization of Weights

--- Network.java
+++ Network.java
@@ -6,19 +6,36 @@ import java.util.Random;
 public class Network {
 
 	int numLayers = 0;
+	
+	int[] sizes;
+	
+	/** 
+	 * Biases is a two-dimensional array, with the first dimension indicating
+	 * the layer we are referring to and the second the neuron within that layer 
+	 */
+	
 	double[][] biases;
 	
-	public Network(int[] sizes) {
+	/** 
+	 * Weights has three dimensions. The first indicates which two layers these 
+	 * weights are between. The third index represents a neuron from the first of
+	 * those two layers, the second index which neuron from the second layer it is
+	 * connected to (this reversed indexing simplifies the math involved). 
+	 */
+	double[][][] weights;
+	
+	public Network(int[] argSizes) {
 		
 		//Initialize the network with a Gaussian distribution (mean 0, variance 1) 
 		// of random biases and weights
-		
+		sizes = argSizes;
 		numLayers = sizes.length;
 		int[] internal = Arrays.copyOfRange(sizes, 1, sizes.length);
 		
-		//Initialize biases
+		//Randomly initialize biases
 		
-		biases = new double[internal.length][];
+		biases = new double[numLayers - 1][];
+		weights = new double[numLayers - 1][][];
 		int biasIndex = 0;
 		Random rand = new Random();
 		for(int size : internal) {
@@ -29,8 +46,17 @@ public class Network {
 			biasIndex++;
 		}
 		
-		//Initialize weights (TO-DO)
+		//Randomly initialize weights
 		
+		for (int i = 0; i < numLayers - 1; i++) {
+			weights[i] = new double[sizes[i+1]][];
+			for(int j = 0; j < sizes[i+1]; j++) {
+				weights[i][j] = new double[sizes[i]];
+				for (int k = 0; k < sizes[i]; k++) {
+					weights[i][j][k] = rand.nextGaussian();
+				}
+			}
+		}	
 	}
 	
 	public static void printBiases(Network net) {
@@ -42,9 +68,21 @@ public class Network {
 		}
 	}
 	
+	public static void printWeights(Network net) {
+		//Print weights between the second and third layers and from there on
+		for (int i = 1; i < net.numLayers - 1; i++) {
+			for (int j = 0; j < net.sizes[i+1]; j++) {
+				System.out.println ("New Neuron");
+				for (int k = 0; k < net.sizes[i]; k++) {
+					System.out.println(net.weights[i][j][k]);
+				}
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		int[] sizes = new int[]{784, 15, 10};
 		Network thisNet = new Network(sizes);
-		printBiases(thisNet);
+		printWeights(thisNet);
 	}
 }
